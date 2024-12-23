987580a878daee4057e84c3bb79fab156184bdf1
bokigolic <bokigolic33@gmail.com>
2024-12-22 20:15:54 -0500

    added movieCard component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MovieCard from './components/MovieCard';
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieCard movie={{ title: "Tim's film", release_date: "2024" }} />
     </div>
   );
 }
