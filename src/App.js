dd4162432c6cd9de4efef332d97255201adbe4b7
bokigolic <bokigolic33@gmail.com>
2024-12-22 20:24:23 -0500

    work on pages, rotes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import MovieCard from './components/MovieCard';
 
 
 function App(props) {
+
+
+
   return (
-    <div className="App">
-      <MovieCard movie={{ title: "Tim's film", release_date: "2024" }} />
-    </div>
+    <>
+
+    </>
   );
 }
 
